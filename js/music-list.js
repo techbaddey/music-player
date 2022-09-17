a2424ba9610f66e8add57241e3f17ceefe048239
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-09-17 08:21:37 +0100

    updated

--- js/music-list.js
+++ js/music-list.js
@@ -35,5 +35,41 @@ let allMusic = [
     img: "music-6",
     src: "music-6"
   },
+  {
+    name: "Echo",
+    artist: "Tauren Wells",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Promises",
+    artist: "Maverick City Music",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Famous For",
+    artist: "Tauren Wells ft. Jenn Johnson",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "This I Believe",
+    artist: "Hillsong Worship",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Through It All",
+    artist: "Planetshakers",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Won't Let Go",
+    artist: "Travis Greene",
+    img: "music-12",
+    src: "music-12"
+  },
   
 ];
